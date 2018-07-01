bd20ba3ff261de67ccc80a21f894b04df1f95286
ryanoliversotto <ryan.sotto@outlook.com>
2018-07-01 10:49:03 -0500

    Cloned the ORM|MVC version of code

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+### Seeds Sql
+
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Iceburg Wrapped Grass Fed Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Hickory Chicken Siracha Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Ramen Bun Sweet Teriyaki Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Tempeh Bun Black Bean Vegan Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Mini Sliders Cheddar and Beef Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Tandori Fired Salmon Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Orange Glazed Duck Breast Burger');
